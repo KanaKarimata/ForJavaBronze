9290ba189c76e8c86c0956dec95a99561725eb75
KanaKarimata <kana.lt22@gmail.com>
2022-10-06 17:31:55 +0900

    リテラル

--- src/example/com/main/Main.java
+++ src/example/com/main/Main.java
@@ -2,6 +2,16 @@ package example.com.main;
 
 public class Main {
     public static void main(String[] args) {
-
+        System.out.println("Hello World!");
+        System.out.println(255);
+        System.out.println(0377);
+        System.out.println(0xff);
+        System.out.println(0b101);
+        System.out.println(12.33);
+        System.out.println(3e4);
+        System.out.println('A');
+        System.out.println('\u3012');
+        System.out.println("Hello");
+        System.out.println(true);
     }
 }
