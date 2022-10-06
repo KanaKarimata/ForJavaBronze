345d0b9b700762225aba4ddbb11cb4d6312b483c
KanaKarimata <kana.lt22@gmail.com>
2022-10-06 18:18:21 +0900

    識別子と予約語

--- src/example/com/main/Main.java
+++ src/example/com/main/Main.java
@@ -2,16 +2,38 @@ package example.com.main;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
-        System.out.println(255);
-        System.out.println(0377);
-        System.out.println(0xff);
-        System.out.println(0b101);
-        System.out.println(12.33);
-        System.out.println(3e4);
-        System.out.println('A');
-        System.out.println('\u3012');
-        System.out.println("Hello");
-        System.out.println(true);
+//        System.out.println("Hello World!");
+//        System.out.println(255);
+//        System.out.println(0377);
+//        System.out.println(0xff);
+//        System.out.println(0b101);
+//        System.out.println(12.33);
+//        System.out.println(3e4);
+//        System.out.println('A');
+//        System.out.println('\u3012');
+//        System.out.println("Hello");
+//        System.out.println(true);
+//        byte b = 10;
+//        short s = 20;
+//        int i = 30;
+//        long l = 40L;
+//        float f = 1.15F;
+//        double d = 5.78;
+//        char a ='A';
+//        boolean o = true;
+//
+//        System.out.println("byte :" + b);
+//        System.out.println("short: " + s);
+//        System.out.println("int: " + i);
+//        System.out.println("long: " + l);
+//        System.out.println("float: " + f);
+//        System.out.println("double: " + d);
+//        System.out.println("char: " + a);
+//        System.out.println("boolean:" + o);
+        String s1 = "tanaka";
+        int num = s1.length();
+        char c = s1.charAt(2);
+        System.out.println(num);
+        System.out.println(c);
     }
 }
