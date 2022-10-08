a928ad391b126e113bd02810647ba65223121970
KanaKarimata <kana.lt22@gmail.com>
2022-10-08 23:44:30 +0900

    クラスの作成

--- src/example/com/main/Main.java
+++ src/example/com/main/Main.java
@@ -212,7 +212,25 @@ public class Main {
 ////            }
 //        }
 
+        Employee a = new Employee();
+        a.setId(100);
 
+        Employee b = new Employee();
+        b.setId(200);
 
+        System.out.println("Aさん" + a.getId());
+        System.out.println("Bさん" + b.getId());
+
+    }
+}
+
+class Employee {
+    int id;
+    void setId(int i) {
+        id = i;
+    }
+
+    int getId() {
+        return id;
     }
 }
